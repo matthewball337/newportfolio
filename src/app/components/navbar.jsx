cb67340f408b2ec6af179a7888336af6ff91f10b
Matthew Ball <matthewball337@gmail.com>
2025-02-06 10:49:00 -0800

    new portfolio

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3078e4]">
+
+        </div>
+    )
+}
+
+export default Navbar
