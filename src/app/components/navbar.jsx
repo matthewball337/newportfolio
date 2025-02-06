3a7cc4dcd4ce307854f32843b60a1af23263f024
Matthew Ball <matthewball337@gmail.com>
2025-02-06 12:12:40 -0800

    feat: added fa bars to react icons
    make sure to do "npm install react-icons --save" in order to include them

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-// import {FaBars, FaTimes} from 'react-icons/fa'
-
-function Navbar() {
+import {FaBars} from 'react-icons/fa'
+// import FaTimes from 'react-icons/fa'
+const Navbar = () => {
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3078e4] text-gray-300">
             <div>
                 <img src='./logo.png' alt="Logo Image" style={{width: '50px'}}/>
             </div>
+
+            {/* menu */}
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+            {/* Hamburger */}
+                <div>
+                    <FaBars />
+                </div>
+                {/* Mobile menu */}
+                <div></div>
+
+                {/* Social icons */}
+                <div></div>
         </div>
     )
 }
