312c25f16d6dfc8a35bfaefa4c264e077c1bb09b
Matthew Ball <matthewball337@gmail.com>
2025-02-06 11:50:07 -0800

    updated profile logo

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Logo from '../assets/logo.png'
 // import {FaBars, FaTimes} from 'react-icons/fa'
 
 function Navbar() {
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3078e4] text-gray-300">
             <div>
-                <img src={Logo} alt="Logo Image" />
+                <img className="relative w-auto h-[60px] place-self-center" src='./logo.png' alt="Logo Image" />
             </div>
         </div>
     )
