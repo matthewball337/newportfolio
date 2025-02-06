2ffa9465333fe5b032d2d9dbab71ba07abf28116
Matthew Ball <matthewball337@gmail.com>
2025-02-06 11:30:50 -0800

    linked logo image in navbar, does not load

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from '../assets/logo.png'
+// import {FaBars, FaTimes} from 'react-icons/fa'
 
 function Navbar() {
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3078e4]">
-
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3078e4] text-gray-300">
+            <div>
+                <img src={Logo} alt="Logo Image" />
+            </div>
         </div>
     )
 }
