9dda93877b4e973cdb0041823fa5d2424699c156
Matthew Ball <matthewball337@gmail.com>
2025-02-10 10:16:13 -0500

    updated color of navigation bar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, {useState} from 'react'
 import {FaBars} from 'react-icons/fa'
 // import FaTimes from 'react-icons/fa'
@@ -5,9 +7,9 @@ const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav);
     return (
-        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#77a2e6] text-black">
             <div>
-                <img src='./assets/logo.png' alt="Logo Image" style={{width: '50px'}}/>
+                <img src='./assets/logo.png' alt="Logo Image" style={{width: '150px'}}/>
             </div>
 
             {/* menu */}
@@ -24,7 +26,7 @@ const Navbar = () => {
                 <FaBars />
             </div>
             {/* Mobile menu */}
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#77a2e6] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
