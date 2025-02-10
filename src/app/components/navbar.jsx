10997f211456b0095768a612c54b7290f18a6a91
Matthew Ball <matthewball337@gmail.com>
2025-02-10 10:25:18 -0500

    created homepage, updated light blue color to electric blue

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,13 +1,13 @@
 'use client'
 
 import React, {useState} from 'react'
-import {FaBars} from 'react-icons/fa'
-// import FaTimes from 'react-icons/fa'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav);
     return (
-        <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#77a2e6] text-black">
+        <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#7df9ff] text-black">
             <div>
                 <img src='./assets/logo.png' alt="Logo Image" style={{width: '150px'}}/>
             </div>
@@ -23,10 +23,10 @@ const Navbar = () => {
 
             {/* Hamburger */}
             <div onClick={handleClick} className='md:hidden z-10'>
-                <FaBars />
+                {!nav ? <FaBars /> : <FaTimes />}
             </div>
             {/* Mobile menu */}
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#77a2e6] flex flex-col justify-center items-center'}>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#7df9ff] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
