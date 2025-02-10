aca7ed9fa6eeeb57e0fce1f4c9d9bf3be1e4dde2
Matthew Ball <matthewball337@gmail.com>
2025-02-10 12:55:08 -0500

    updated list elements

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -37,24 +37,24 @@ const Navbar = () => {
             </ul>
 
             {/* Social icons */}
-            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <div className='flex fixed flex-col top-[35%] left-0'>
                 <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-blue-600'>
+                    <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-blue-600'>
                         <a className='flex justify-between items-center w-full text-gray-300'
                         href="/">
-                            Linkedin <FaLinkedin size={30} />
+                            Linkedin <FaLinkedin className="mr-[-10px] lg:mr-0 duration-300" size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#333333]'>
+                    <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#333333]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
                         href="/">
-                            Github <FaGithub size={30} />
+                            Github <FaGithub className="mr-[-10px] lg:mr-0 duration-300" size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#6fc2b0]'>
+                    <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#6fc2b0]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
                         href="/">
-                            Email <HiOutlineMail size={30} />
+                            Email <HiOutlineMail className="mr-[-10px]" size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#565f69]'>
