1c153af6f9feb8d1bf027f78c3c0d47c0d2fd72e
Matthew Ball <matthewball337@gmail.com>
2025-02-10 09:15:39 -0500

    new logo added, mobile navigation bar information updated

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
-                <img src='./logo.png' alt="Logo Image" style={{width: '50px'}}/>
+                <img src='./assets/logo.png' alt="Logo Image" style={{width: '50px'}}/>
             </div>
 
             {/* menu */}
@@ -24,7 +24,7 @@ const Navbar = () => {
                 <FaBars />
             </div>
             {/* Mobile menu */}
-            <ul className={!nav ? 'hidden' : ''}>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
