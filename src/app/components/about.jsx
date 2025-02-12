2e1f0ce7617e44448b2cf162b44fddec52c08744
Matthew Ball <matthewball337@gmail.com>
2025-02-12 15:19:16 -0500

    full width

--- src/app/components/about.jsx
+++ src/app/components/about.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='max-w-[1000px]'>
+            <div className='max-w-[1000px] w-full'>
                 <div>
                     <p>About</p>
                 </div>
