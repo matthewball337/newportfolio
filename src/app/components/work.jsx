0d3c3b8ab94ace9a8824f8164c13cdf3aea7e49d
Matthew Ball <matthewball337@gmail.com>
2025-02-14 15:57:37 -0500

    work changes

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -5,7 +5,7 @@ const Work = () => {
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p>Work</p>
+                <p className='text-4xl'>Work</p>
                 <p>// Check out some of my recent work</p>
             </div>
 
