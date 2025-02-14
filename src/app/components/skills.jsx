63ac0ed0ad2cd0b0931cdd16b427661dd0ed78a6
Matthew Ball <matthewball337@gmail.com>
2025-02-14 09:01:37 -0500

    imported images of skills

--- src/app/components/skills.jsx
+++ src/app/components/skills.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import CPlusPlus from "./assets/cplusplus.png"
+import GitHub from "./assets/github.png"
+import Node from "./assets/node.png"
+import Python from "./assets/python.png"
+import ReactImage from "./assets/react.png"
+import WordPress from "./assets/wordpress.png"
+
 const Skills = () => {
   return (
     <div>Skills</div>
