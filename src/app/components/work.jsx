2cf4637bdb6bd93bd3ab727eb9e29e0511e1e849
Matthew Ball <matthewball337@gmail.com>
2025-02-14 15:45:28 -0500

    worked on button in work

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div>
+            <div>
+                <p>Work</p>
+                <p>// Check out some of my recent work</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
