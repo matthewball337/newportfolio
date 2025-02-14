395dd51ccea1070942c3a99e038352143a85e1b3
Matthew Ball <matthewball337@gmail.com>
2025-02-14 09:16:30 -0500

    added C++ skill

--- src/app/components/skills.jsx
+++ src/app/components/skills.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 
-import CPlusPlus from "./assets/cplusplus.png"
-import GitHub from "./assets/github.png"
-import Node from "./assets/node.png"
-import Python from "./assets/python.png"
-import ReactImage from "./assets/react.png"
-import WordPress from "./assets/wordpress.png"
+//import GitHub from "./assets/github.png"
+//import Node from "./assets/node.png"
+//import Python from "./assets/python.png"
+//import ReactImage from "./assets/react.png"
+//import WordPress from "./assets/wordpress.png"
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills'>
+      {/* Container */}
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>// These are the technologies I've worked with</p>
+        </div>
+
+        <div>
+          <div>
+            <img src='./assets/cplusplus.png' alt="C++ icon" />
+            <p>C++</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from './components/about'
 import Home from "./components/home"
 import Navbar from "./components/navbar"
+import Skills from "./components/skills"
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </>
   );
 }
