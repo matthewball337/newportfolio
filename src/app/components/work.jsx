8af1f19b9445e450c99ec9d31a6a58dff5f874f3
Matthew Ball <matthewball337@gmail.com>
2025-02-14 15:54:19 -0500

    updated class info of work divs

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-        <div>
-            <div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
                 <p>Work</p>
                 <p>// Check out some of my recent work</p>
             </div>
