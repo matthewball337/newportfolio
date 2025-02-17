a7f2be7749e01cff429a4e8ac6c708c9f61ca275
Matthew Ball <matthewball337@gmail.com>
2025-02-17 09:12:47 -0500

    work image added
    content: Las Vegas Rehearsal Space page added as work image, also hover effect comment added to work.jsx

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -11,6 +11,8 @@ const Work = () => {
 
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                    {/* Hover Effects */}
                     <div>
                         <span>
 
