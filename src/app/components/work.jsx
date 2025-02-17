120287af4ca7956d7400e37e0e2360084bd97b20
Matthew Ball <matthewball337@gmail.com>
2025-02-17 11:32:20 -0500

    hover effect for work

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -14,16 +14,16 @@ const Work = () => {
                 <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                     {/* Hover Effects */}
-                    <div>
-                        <span>
-
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+WordPress Application
                         </span>
-                        <div>
+                        <div className='pt-8 text-center'>
                             <a href="/">
-                                <button></button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button></button>
+                                <button>Code</button>
                             </a>
                         </div>
                     </div>
