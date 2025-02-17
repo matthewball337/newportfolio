a93aedd0de4ed454772eef1204ccb3622b46ff59
Matthew Ball <matthewball337@gmail.com>
2025-02-17 09:01:04 -0500

    defined the first work paragraph style

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -6,11 +6,11 @@ const Work = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-black'>Work</p>
-                <p>// Check out some of my recent work</p>
+                <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
-            <div>
-                <div>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     <div>
                         <span>
 
