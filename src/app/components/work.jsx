7eb99bef508b5436c4287d6cb9da638f15ff3984
Matthew Ball <matthewball337@gmail.com>
2025-02-19 09:04:37 -0500

    feat: added formatting to Code button in Work component

--- src/app/components/work.jsx
+++ src/app/components/work.jsx
@@ -23,7 +23,7 @@ WordPress Application
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href="/">
-                                <button>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
                     </div>
