e4cc72cd59e09f3c8271ac12ecd8b393cfee95bf
Matthew Ball <matthewball337@gmail.com>
2025-02-21 11:07:40 -0500

    feat: set up contact form class

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
-        <form action="">
+        <form action="" className="flex flex-col max-w-[600px] w-full">
             <div>
                 <p>Contact</p>
             </div>
