10e1ee6f407669b1d7885cb5e7361c4cc35848d2
Matthew Ball <matthewball337@gmail.com>
2025-02-21 11:04:55 -0500

    feat: created contact form header

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='w-full h-screen bg-[#0096ff]'>
-
+    <div name='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
+        <form action="">
+            <div>
+                <p>Contact</p>
+            </div>
+        </form>
     </div>
   )
 }
