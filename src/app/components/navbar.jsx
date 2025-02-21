623b05a3deedbd563f319111e1e001ab74355214
Matthew Ball <matthewball337@gmail.com>
2025-02-21 10:53:30 -0500

    feat: links added to navbar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -4,6 +4,7 @@ import React, {useState} from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import Link from 'next/link'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -40,28 +41,28 @@ const Navbar = () => {
             <div className='flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-blue-600'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href="/">
+                        <Link className='flex justify-between items-center w-full text-gray-300'
+                        href="https://www.linkedin.com/in/matthew-joseph-ball/">
                             Linkedin <FaLinkedin className="mr-[-10px] lg:mr-0 duration-300" size={30} />
-                        </a>
+                        </Link>
                     </li>
                     <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#333333]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
-                        href="/">
+                        <Link className='flex justify-between items-center w-full text-gray-300'
+                        href="https://github.com/matthewball337">
                             Github <FaGithub className="mr-[-10px] lg:mr-0 duration-300" size={30} />
-                        </a>
+                        </Link>
                     </li>
                     <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#6fc2b0]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
+                        <Link className='flex justify-between items-center w-full text-gray-300'
                         href="/">
                             Email <HiOutlineMail className="mr-[-10px] lg:mr-0 duration-300" size={30} />
-                        </a>
+                        </Link>
                     </li>
                     <li className='w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100] hover:ml-[-10] duration-300 bg-[#565f69]'>
-                        <a className='flex justify-between items-center w-full text-gray-300'
+                        <Link className='flex justify-between items-center w-full text-gray-300'
                         href="/">
                             Resume <BsFillPersonLinesFill className="mr-[-10px] lg:mr-0 duration:300" size={30} />
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
