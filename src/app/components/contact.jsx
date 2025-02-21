8a7761e4b89775bcacaf2b8e032a5209832128eb
Matthew Ball <matthewball337@gmail.com>
2025-02-21 11:22:20 -0500

    feat: contact section formatting added

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
         <form action="" className="flex flex-col max-w-[600px] w-full">
-            <div>
-                <p>Contact</p>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-black text-gray-300'>Contact</p>
             </div>
         </form>
     </div>
