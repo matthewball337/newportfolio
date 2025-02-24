259833a6793aae74b39472e0af989c3397900a3d
Matthew Ball <matthewball337@gmail.com>
2025-02-24 11:37:02 -0500

    feat: name field added to form

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-black text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - matthewball337@gmail.com</p>
             </div>
+            <input type="text" placeholder='Name' name='name' />
         </form>
     </div>
   )
