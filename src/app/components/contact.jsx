a6d3efecb24958ceec7ba0928aa3643471960b4d
Matthew Ball <matthewball337@gmail.com>
2025-02-24 11:29:42 -0500

    feat: submit the form below text added

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
         <form action="" className="flex flex-col max-w-[600px] w-full">
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-black text-gray-300'>Contact</p>
+                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - matthewball337@gmail.com</p>
             </div>
         </form>
     </div>
