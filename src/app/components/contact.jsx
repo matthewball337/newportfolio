05278f40ca659efd114c81bb0ecefa7d993c44ff
Matthew Ball <matthewball337@gmail.com>
2025-02-26 09:19:22 -0500

    feat: textarea on contact form for message created

--- src/app/components/contact.jsx
+++ src/app/components/contact.jsx
@@ -8,8 +8,9 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-black text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - matthewball337@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='email' />
+            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
         </form>
     </div>
   )
