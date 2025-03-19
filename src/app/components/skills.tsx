a308ff8fb106882aaf1ecba2648f54e72d679e64
Matthew Ball <matthewball337@gmail.com>
2025-03-19 10:19:36 -0400

    feat: bpos swapped out for 92 char code

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-black'>Skills</p>
-          <p className='py-4'>&bsol;&bsol; These are the technologies I&apos;ve worked with</p>
+          <p className='py-4'>&#92;&#92; These are the technologies I&apos;ve worked with</p>
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
