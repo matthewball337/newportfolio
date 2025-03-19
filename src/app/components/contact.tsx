903db76fa981ae3e22f04a3855b3e5dd872bf880
Matthew Ball <matthewball337@gmail.com>
2025-03-19 15:34:51 -0400

    feat: getform io implemented

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,122 +1,27 @@
-'use client'
-
-import React, {useState} from 'react'
-import {track} from "@vercel/analytics/react"
-type submissionStatus = "none" | "success" | "error";
+import React from 'react'
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [submission, setSubmission] = useState<{
-    status: submissionStatus;
-    message: string;
-  }>({ status: "none", message: ""});
-
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleChange = (e: any) => { //eslint-disable-line
-    const {name, value} = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: any) => { //eslint-disable-line
-    e.preventDefault();
-
-    track("Contact Form");
-    setSubmitting(true);
-
-    const res = await fetch("/api/mail", {
-      body: JSON.stringify({
-        name: formData.name,
-        email: formData.email,
-        message: formData.message,
-      }),
-      headers: {
-        "Content-Type": "application.json",
-      },
-      method: "POST",
-    });
-
-    const {status, message} = await res.json();
-
-    if (status === "error") {
-      setSubmission({ status, message });
-      return;
-    } else {
-      setSubmission({ status, message });
-    }
-
-    setSubmitting(false);
-  }
   return (
     <div id='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
-      <form onSubmit={handleSubmit} action="" className="flex flex-col items-center max-w-[600px] w-full">
+      <form method='POST' action="https://getform.io/f/aroypqmb"
+        className="flex flex-col max-w-[600px] w-full">
         <div className='pb-8'>
-          <p className='text-4xl font-bold border-b-4 border-black text-gray-300 flex justify-center'>Contact</p>
-          <p className='text-gray-300 py-4 max-w-[200px] justify-center'>
+          <p className='text-4xl font-bold border-b-4 border-black text-gray-300 text-center'>Contact</p>
+          <p className='text-gray-300 py-4'>
             &#92;&#92;Submit the form below or shoot me an email - matthewball337@gmail.com
           </p>
         </div>
-        {submission.status === "error" && (
-          <div className="py-12 text-red-300">Submission failed</div>
-        )}
-          {submission.status === "success" && (
-            <div className="py-12 text-green-300">Submission succeeded</div>
-          )}
-        {submission.status === "none" && (
-          <div className="justify-center">
-            <div>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Name"
-                required
-                className='bg-[#ccd6f6] p-2'
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-                required
-                className='my-4 p-2 bg-[#ccd6f6]'
-              />
-            </div>
-            <div>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                placeholder="Message"
-                rows={10}
-                required
-                className='bg-[#ccd6f6] p-2'
-              />
-            </div>
-            <button
-              className='text-white border-2 hover:bg-black hover:border-black px-4 py-3 my-8 mx-auto flex items-center'
-              disabled={submitting}
-              type="submit"
-            >
-              Let&apos;s Collaborate
-            </button>
-          </div>
-        )};
+        <input type="text" name="name" placeholder="Name"
+        className='bg-[#ccd6f6] p-2' />
+        <input type="text" name="email" placeholder="Email"
+        className='my-4 p-2 bg-[#ccd6f6]' />
+        <textarea name="message" placeholder="Message" rows={10}
+        className='bg-[#ccd6f6] p-2' />
+        <button
+        className='text-white border-2 hover:bg-black hover:border-black px-4 py-3 my-8 mx-auto flex items-center'
+        type="submit">
+          Let&apos;s Collaborate
+        </button>
       </form>
     </div>
   )
