2c237870364c93884ca28bc42c077bd836844fd6
Matthew Ball <matthewball337@gmail.com>
2025-03-21 08:48:55 -0400

    feat: fixed jumping point of Contact selection

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -4,10 +4,10 @@ const Contact = () => {
   return (
     <div id='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
       <form method='POST' action="https://getform.io/f/aroypqmb"
-        className="flex flex-col max-w-[600px] w-full">
+        className="mx-auto p-4 flex flex-col justify-center h-full max-w-[600px] w-full">
         <div className='pb-8'>
-          <p className='text-4xl font-bold border-b-4 border-black text-gray-300 text-center'>Contact</p>
-          <p className='text-gray-300 py-4'>
+          <p className='text-4xl font-bold border-b-4 border-black text-gray-300 text-center'><br /><br />Contact</p>
+          <p className='text-gray-300 py-4 text-center'>
             &#92;&#92;Submit the form below or shoot me an email - matthewball337@gmail.com
           </p>
         </div>
