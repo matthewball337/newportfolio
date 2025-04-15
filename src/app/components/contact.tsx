b6b088f99e85c7e8678ffc83819bc41a184b0df2
matthewball337 <matthewball337@gmail.com>
2025-04-15 13:28:33 -0400

    feat: contact tsx covers whole section on firefox

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='contact' className='w-full h-screen bg-[#0096ff] flex justify-center items-center p-4'>
+    <div id='contact' className='w-full h-full bg-[#0096ff] flex justify-center items-center p-4'>
       <form method='POST' action="https://getform.io/f/aroypqmb"
         className="mx-auto p-4 flex flex-col justify-center h-full max-w-[600px] w-full">
         <div className='pb-8'>
@@ -12,11 +12,11 @@ const Contact = () => {
           </p>
         </div>
         <input type="text" name="name" placeholder="Name"
-        className='bg-[#ccd6f6] p-2' />
+        className='bg-[#ccd6f6] p-2' required />
         <input type="text" name="email" placeholder="Email"
-        className='my-4 p-2 bg-[#ccd6f6]' />
+        className='my-4 p-2 bg-[#ccd6f6]' required />
         <textarea name="message" placeholder="Message" rows={10}
-        className='bg-[#ccd6f6] p-2' />
+        className='bg-[#ccd6f6] p-2' required />
         <button
         className='text-white border-2 hover:bg-black hover:border-black px-4 py-3 my-8 mx-auto flex items-center'
         type="submit">
