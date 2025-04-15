49495f5f3948547b952a2b74adf491b326cfa465
Matthew Ball <matthewball337@gmail.com>
2025-04-15 11:10:55 -0400

    feat: navbar is above all of the elements on page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleClick = () => setNav(!nav);
     return (
-        <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0096ff] text-gray-200">
+        <div className="fixed w-full h-[100px] flex justify-between items-center px-4 z-10 bg-[#0096ff] text-gray-200">
             <div className="flex grid-cols-2">
                 <img className="invert" src='./assets/logo.png'
                     alt="Logo Image" style={{width: '150px'}}/>
