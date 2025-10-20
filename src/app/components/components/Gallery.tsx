5986f420818b6fa47a7a7cc65d5564fa000ff963
matthewball337 <matthewball337@gmail.com>
2025-10-20 08:32:59 -0400

    feat: gallery component continued development

--- src/app/components/components/Gallery.tsx
+++ src/app/components/components/Gallery.tsx
@@ -18,6 +18,23 @@ export default function Gallery({galleryImages = "[]"}: GalleryProps) {
 
     const [currentImage, setCurrentImage] = useState(0);
 
+
+    function prevImage() {
+        if(currentImage === 0) {
+            setCurrentImage(galleryImages.length - 1);
+        } else {
+            setCurrentImage(currentImage - 1);
+        }
+    }
+
+    function nextImage() {
+        if(currentImage === (myGallery.length - 1)) {
+            setCurrentImage(0);
+        } else {
+            setCurrentImage(currentImage + 1);
+        }
+    }
+
     return (
         <div className="fixed inset-0 w-screen h-screen bg-[#0096ff]">
             <div className="text-white md:w-[450px] w-full leading-2 h-fit self-center">
@@ -39,8 +56,17 @@ export default function Gallery({galleryImages = "[]"}: GalleryProps) {
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}    
             >
-                <button className="h-[44px] w-[44px]">
+                <button className="h-[44px] w-[44px]" onClick={() => prevImage()}>
                     <HiChevronLeft />
+                </button>
+            </motion.div>
+            <motion.div
+                className="fixed top-[50%] md:right-4 right-1 text-white font-extrabold"
+                whileHover={{scale: 1.1}}
+                whileTap={{ scale: 0.9 }}
+            >
+                <button className="h-[44px] w-[44px]" onClick={() => nextImage()}>
+                    <HiChevronRight />
                 </button>
             </motion.div>
         </div>
